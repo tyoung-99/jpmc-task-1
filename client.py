70f2e03cba98dd12ef1c63097993ecd840d74b4e
Tristan <127442375+tyoung-99@users.noreply.github.com>
2023-03-10 16:33:05 -0500

    Updated stock names in ratio calculation to be dynamic, not hardcoded.

--- client.py
+++ client.py
@@ -51,9 +51,12 @@ if __name__ == "__main__":
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
+        stocks = []
         prices = {}
         for quote in quotes:
-            stock, bid_price, ask_price, prices[stock] = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, prices[stock]))
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            stocks.append(stock)
+            prices[stock] = price
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
+        print("Ratio %s" % getRatio(prices[stocks[0]], prices[stocks[1]]))
